904c63ce34caef80f267d4eeaecc1f488d15e1e4
Rishi <rishibhargawa47@gmail.com>
2025-04-03 13:14:19 +0530

    removed

--- Shell-scripting/sc1.sh
+++
@@ -1,9 +0,0 @@
-echo "Provide Details:"
-echo -n "Your Name: "
-read name
-echo -n "Your Program: "
-read prog
-echo -n "Your Enrollment Number: "
-read enroll
-
-echo "Hello $name, your program is $prog and Enrollment is: $enroll"
